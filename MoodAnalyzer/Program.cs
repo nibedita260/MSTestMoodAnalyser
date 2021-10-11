48b9ed931da897dabeb5846d0bc13742d8a0b0f1
nibedita260 <deepi.nibedita260.com>
2021-10-11 13:18:31 +0530

    [Nibedita] Add: CreateAndRespondMoodAnalyser MStestingMoodAnalyser Features Implemented

---
+++ MoodAnalyzer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MoodAnalyzer
+{
+    class Program
+    {
+        public const string HAPPY_MOOD = "Happy";
+        public const string SAD_MOOD = "Sad";
+        class MoodAnalyser
+        {
+            public void MoodAnalyze(string mood)
+            {
+                if (mood.Contains(HAPPY_MOOD))
+                {
+                    Console.WriteLine("I am in Happy mood");
+
+                    return;
+                }
+                if (mood.Contains(SAD_MOOD))
+                {
+                    Console.WriteLine("I am in Sad mood");
+                    return;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            bool isExit = false;
+            int options;
+            while (!isExit)
+            {
+                Console.WriteLine("choose 1.CreateAndRespondMoodAnalyser");
+                options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        MoodAnalyser moodAnalyser = new MoodAnalyser();
+                        moodAnalyser.MoodAnalyze(HAPPY_MOOD);
+                        break;
+                    default:
+                        Console.WriteLine("choose valid options");
+                        break;
+                }
+            }
+
+        }
+    }
+}
