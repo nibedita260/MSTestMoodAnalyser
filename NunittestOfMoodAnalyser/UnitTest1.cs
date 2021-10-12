90654a68a2bd58ea938764caf320c633d4b2c50b
nibedita260 <deepi.nibedita260.com>
2021-10-13 00:37:34 +0530

    [Nibedita] Add: RefactorUC-4-NCustomException unitTestCase MoodAnalyser using constructor Features Implemented

--- NunittestOfMoodAnalyser/UnitTest1.cs
+++ NunittestOfMoodAnalyser/UnitTest1.cs
@@ -5,31 +5,59 @@ namespace NunittestOfMoodAnalyser
 {
     public class Tests
     {
-        public const string HAPPY_MOOD = "I am in Happy Mood";
-        public const string SAD_MOOD = "I am in Sad Mood";
+        const string HAPPY_MOOD = "Happy";
+        const string SAD_MOOD = "Sad";
+        const string NULLMsg = "Mood can't be Null";
+        const string EmptyMsg = "Mood should not Empty";
         [Test]
-        public void AnalyseForHappyMood()
+        //Test Case-2.1
+        public void Analyse_Happy_Mood()
         {
-            MoodAnalyser mood = new MoodAnalyser(HAPPY_MOOD);
+            MoodAnalyser mood = new MoodAnalyser("I am in happy mood");
             string ActualResult = mood.MoodAnalyze();
-            string ExpectedResult= "Happy";
+            string ExpectedResult = HAPPY_MOOD;
             Assert.AreEqual(ExpectedResult, ActualResult);
         }
         [Test]
-        public void AnalyseForSadMood()
+        //Test Case-2.2
+        public void Analyse_Sad_Mood()
         {
-            MoodAnalyser mood = new MoodAnalyser(SAD_MOOD);
+            MoodAnalyser mood = new MoodAnalyser("I am in sad mood");
             string ActualResult = mood.MoodAnalyze();
-            string ExpectedResult = "Sad";
+            string ExpectedResult = SAD_MOOD;
             Assert.AreEqual(ExpectedResult, ActualResult);
         }
         [Test]
-        public void AnalyseForNullMood()
+        //Test Case 3
+        public void Analyse_Null_Mood()
         {
-            MoodAnalyser mood = new MoodAnalyser("");//null mood
+            MoodAnalyser mood = new MoodAnalyser("null");//null mood
             string ActualResult = mood.MoodAnalyze();
-            string ExpectedResult = "Happy";
-            Assert.AreEqual(ExpectedResult, ActualResult);//Happy,Happy 
+            string ExpectedResult = NULLMsg;
+            Assert.AreEqual(ExpectedResult, ActualResult); 
+        }
+        [Test]
+        //Test Case 4
+        public void Analyse_Given_Empty_Mood()
+        {
+            MoodAnalyser mood = new MoodAnalyser("");//empty mood
+            string ActualResult = mood.MoodAnalyze();
+            string ExpectedResult = EmptyMsg;
+            Assert.AreEqual(ExpectedResult, ActualResult); 
+        }
+        [Test]
+        //Test Case 4.1
+        public void Analyse_Given_Empty_Mood_custom_throw_Exception()
+        {
+            try
+            {
+                MoodAnalyser mood = new MoodAnalyser("");//empty mood
+                string ActualResult = mood.MoodAnalyze();
+            }
+            catch (MoodAnalyserCustomException ex)
+            {
+                Assert.AreEqual(EmptyMsg, ex.Message);
+            }
         }
     }
 }
