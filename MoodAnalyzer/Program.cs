ab215647c310daedc0ad0ef3bf0d216ea7e09b96
nibedita260 <deepi.nibedita260.com>
2021-10-12 22:54:24 +0530

    [Nibedita] Add: RefactorUC-1.2-NunitTestCase MoodAnalyser using constructor Features Implemented

--- MoodAnalyzer/Program.cs
+++ MoodAnalyzer/Program.cs
@@ -4,21 +4,19 @@ namespace MoodAnalyzer
 {
     public class Program
     {
-        public const string HAPPY_MOOD = "Happy";
-        public const string SAD_MOOD = "Sad";
         static void Main(string[] args)
         {
             bool isExit = false;
             int options;
             while (!isExit)
             {
-                Console.WriteLine("choose 1.CreateAndRespondMoodAnalyser");
+                Console.WriteLine("choose 1.CreateAndRespondMoodAnalyser ");
                 options = Convert.ToInt32(Console.ReadLine());
                 switch (options)
                 {
                     case 1:
                         MoodAnalyser moodAnalyser = new MoodAnalyser();
-                        moodAnalyser.MoodAnalyze(SAD_MOOD);
+                        moodAnalyser.MoodAnalyze("I am in Sad Mood");
                         break;
                     default:
                         Console.WriteLine("choose valid options");
