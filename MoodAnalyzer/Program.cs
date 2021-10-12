66c65b7fd0b95462fcd46a3db649488b0c908156
nibedita260 <deepi.nibedita260.com>
2021-10-12 22:26:45 +0530

    [Nibedita] Add: RefactorUC1 MoodAnalyser using constructor Features Implemented

--- MoodAnalyzer/Program.cs
+++ MoodAnalyzer/Program.cs
@@ -4,21 +4,20 @@ namespace MoodAnalyzer
 {
     class Program
     {
-        public const string HAPPY_MOOD = "Happy";
-        public const string SAD_MOOD = "Sad";
+        public const string HAPPY_MOOD = "I am in Happy mood";
+        public const string SAD_MOOD = "I am in Sad mood";
         class MoodAnalyser
         {
             public void MoodAnalyze(string mood)
             {
                 if (mood.Contains(HAPPY_MOOD))
                 {
-                    Console.WriteLine("I am in Happy mood");
-
+                    Console.WriteLine("Happy");
                     return;
                 }
                 if (mood.Contains(SAD_MOOD))
                 {
-                    Console.WriteLine("I am in Sad mood");
+                    Console.WriteLine("Sad");
                     return;
                 }
             }
@@ -35,7 +34,7 @@ namespace MoodAnalyzer
                 {
                     case 1:
                         MoodAnalyser moodAnalyser = new MoodAnalyser();
-                        moodAnalyser.MoodAnalyze(HAPPY_MOOD);
+                        moodAnalyser.MoodAnalyze(SAD_MOOD);
                         break;
                     default:
                         Console.WriteLine("choose valid options");
